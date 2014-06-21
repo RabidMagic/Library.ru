657017130869e1dc3cad1566a170a755f66f7259
Frolkin <frolkinnikita94@gmail.com>
2014-06-21 20:32:00 +0400

    update fav-book-del.php

--- fav-book-del.php
+++ fav-book-del.php
@@ -1,8 +1,18 @@
 <?php
-require_once 'func.php';
-require_once 'connect.php';
 session_start();
-$query_fav = "DELETE FROM favourites WHERE login='".$_POST['fav-login']."' && book_id='".$_POST['fav-b-id']."'";
-$result_fav = $mdb2->exec($query_fav);
+$filename = 'xml/fav/'.$_SESSION['login'].'.xml';
+$dom = new DOMDocument();
+$dom->load($filename);
+$xml = $dom->documentElement;
+$gettn = $xml->getElementsByTagName('book');
+foreach ($gettn as $value) {
+    if ($value->getAttribute('name') == $_POST['book_id']) {
+        $xml->removeChild($value);
+    }
+}
+if ($xml->firstChild->tagName == NULL) { 
+    unlink($filename); 
+    } else $dom->save($filename);
+$dom->save($filename);
 header("Location: ".$_SERVER['HTTP_REFERER']);
 ?>
