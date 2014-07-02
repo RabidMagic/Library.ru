9a91b8f903a74f135aee59c496ea312035a05c94
Frolkin Nikita <frolkinnikita94@gmail.com>
2014-07-02 15:30:12 +0400

    Правка существующей документации

--- GetResults.php
+++ GetResults.php
@@ -11,54 +11,54 @@ class GetResults {
     private $result;
     private $row;
     private $query;
-    /**
-     * Отправка запроса на выборку
-     * 
-     * @param int Количество выводимых блоков
-     * @param string Запрос
-     * @param MDB2 Объект коннекта к БД
-     */
-    public function __construct($num, $query, $mdb2) {
-        $this->page = $_GET['page'];
-        if(empty($this->page) or $this->page < 0) $this->page = 1;
-        $this->start = $this->page * $num - $num;
-        $this->query = $query . " LIMIT $this->start, $num";
-        $this->result = $mdb2->query($this->query);
-    }
-    /**
-     * Вывод комментариев
-     * 
-     */
-    public function getReview() {
-        if ($this->result->numRows() > 0)
-        {
-            while ($this->row = $this->result->fetchRow())
-            {
-                print "<div class='comment'>
-                            <div class='comment_head'>
-                                <div class='user'>Пользователь: <b>".  $this->row['login']."</b></div> <div class='date'>Дата: <b>".  $this->row['date']."</b></div></div>
-                            <div class='comment_body'>".  $this->row['content']."</div>
-                       </div>";
-            } 
-               
-        } else print "<h3 style='text-align: center;'>Пока здесь нет отзывов, но Вы можете быть первым</h3>";
-    }
-    /**
-     * Вывод результатов поиска
-     */
-    public function getSearchResults() {
-        if ($this->result->numRows() > 0)
-        {
-            while ($this->row = $this->result->fetchRow())
-            {
-                print "<a href='page.php?id=".$this->row['id']."'>
-                        <div id='search'>
-                            <div><h1>".$this->row['author']."</h1></div>
-                            <div><h3>".$this->row['book_name']."</h3></div>
-                            <div>".$this->row['description']."</div>
-                        </div>
-                        </a>";
-            }     
+        /**
+         * Отправка запроса на выборку
+         * 
+         * @param int Количество выводимых блоков
+         * @param string Запрос
+         * @param MDB2 Объект коннекта к БД
+         */
+        public function __construct($num, $query, $mdb2) {
+            $this->page = $_GET['page'];
+            if(empty($this->page) or $this->page < 0) $this->page = 1;
+            $this->start = $this->page * $num - $num;
+            $this->query = $query . " LIMIT $this->start, $num";
+            $this->result = $mdb2->query($this->query);
         }
-    }
+        /**
+         * Вывод комментариев
+         * 
+         */
+        public function getReview() {
+            if ($this->result->numRows() > 0)
+            {
+                while ($this->row = $this->result->fetchRow())
+                {
+                    print "<div class='comment'>
+                                <div class='comment_head'>
+                                    <div class='user'>Пользователь: <b>".  $this->row['login']."</b></div> <div class='date'>Дата: <b>".  $this->row['date']."</b></div></div>
+                                <div class='comment_body'>".  $this->row['content']."</div>
+                           </div>";
+                } 
+
+            } else print "<h3 style='text-align: center;'>Пока здесь нет отзывов, но Вы можете быть первым</h3>";
+        }
+        /**
+         * Вывод результатов поиска
+         */
+        public function getSearchResults() {
+            if ($this->result->numRows() > 0)
+            {
+                while ($this->row = $this->result->fetchRow())
+                {
+                    print "<a href='page.php?id=".$this->row['id']."'>
+                            <div id='search'>
+                                <div><h1>".$this->row['author']."</h1></div>
+                                <div><h3>".$this->row['book_name']."</h3></div>
+                                <div>".$this->row['description']."</div>
+                            </div>
+                            </a>";
+                }     
+            }
+        }
 }
--- OutputSelect.php
+++ OutputSelect.php
@@ -10,36 +10,36 @@ class OutputSelect {
     private $result;
     private $value;
     private $inset;
-    /**
-     * Отправка запроса на выборку
-     * 
-     * @param string Запрос
-     * @param MDB2 Объект коннекта к БД
-     */
-    public function __construct($query, $mdb2) {
-        $this->result = $mdb2->query($query);
-    }
-    /**
-     * 
-     * @param string Значение аттрибута 'name'
-     * @param string Допольнительные теги 'select'
-     */
-    public function getOption($inset, $addinset = NULL) {
-        if ($this->result->numRows() > 0)
-        {
-            $this->inset = '<select name=\'';
-            $this->inset .= $inset;
-            $this->inset .= '\'>';
-            if ($addinset != NULL) $this->inset .= $addinset;
-            print $this->inset;
-            while ($this->row = $this->result->fetchRow())
-            {
-                foreach ($this->row as $this->value)
-                {
-                    print "<option>$this->value</option>\n";
-                }
-            }
-            print '</select>';
+        /**
+         * Отправка запроса на выборку
+         * 
+         * @param string Запрос
+         * @param MDB2 Объект коннекта к БД
+         */
+        public function __construct($query, $mdb2) {
+            $this->result = $mdb2->query($query);
         }
-    }
+        /**
+         * 
+         * @param string Значение аттрибута 'name'
+         * @param string Допольнительные теги 'select'
+         */
+        public function getOption($inset, $addinset = NULL) {
+            if ($this->result->numRows() > 0)
+            {
+                $this->inset = '<select name=\'';
+                $this->inset .= $inset;
+                $this->inset .= '\'>';
+                if ($addinset != NULL) $this->inset .= $addinset;
+                print $this->inset;
+                while ($this->row = $this->result->fetchRow())
+                {
+                    foreach ($this->row as $this->value)
+                    {
+                        print "<option>$this->value</option>\n";
+                    }
+                }
+                print '</select>';
+            }
+        }
 }
