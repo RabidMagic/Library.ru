8b9fdfe3779a7f3038bb36c5d4facb1ec2e91a72
Frolkin Nikita <frolkinnikita94@gmail.com>
2014-07-02 14:51:00 +0400

    Документация для FavBook

--- FavBook.php
+++ FavBook.php
@@ -1,23 +1,42 @@
 <?php
+/**
+ * Избранные книги
+ * 
+ * Класс для управления избранными книгами
+ * 
+ * @package LibraryClasses
+ * @author tervaskanto <frolkinnikita94@gmail.com>
+ */
 class FavBook {
-    protected $filename;
-    protected $xml;
-    protected $string;
-    protected $book;
-    protected $options;
-    protected $stat = FALSE;
-    protected $gettn;
-    protected $dom;
-    protected $id;
-    protected $print;
-    protected $length;
-    protected $i;
-    protected $result;
+    private $filename;
+    private $xml;
+    private $string;
+    private $book;
+    private $options;
+    private $stat = FALSE;
+    private $gettn;
+    private $dom;
+    private $id;
+    private $print;
+    private $length;
+    private $i;
+    private $result;
+            /**
+             * Открывает xml-файл и создаёт объект SimpleXMLElement
+             * 
+             * @param string Имя файла(без расширения)
+             * @param array Дополнительный массив опций
+             * @param string Путь к файлу(без указания имени файла)
+             */
             function __construct($name, $options = array(), $filepath = 'xml/fav/') {
                 $this->filename = $filepath . $name . '.xml';
                 $this->xml = @simplexml_load_file($this->filename);
                 $this->options = $options;
             }
+            /**
+             * Добавление книги в Избранное
+             * 
+             */
             public function addFav() {
                 if ($this->xml === FALSE) {
                     $this->string = '<?xml version="1.0" encoding="windows-1251" standalone="yes"?>';
@@ -32,6 +51,9 @@ class FavBook {
                 if (@scandir('xml/fav') == FALSE) mkdir('xml/fav');
                 if ($this->xml->asXML($this->filename)) $this->stat = TRUE;
             }
+            /**
+             * Удаление книги из Избранного
+             */
             public function delFav() {
                 $this->dom = new DOMDocument();
                 $this->dom->load($this->filename);
@@ -49,16 +71,25 @@ class FavBook {
                     unlink($this->filename);
                 }
             }
+            /**
+             * Проверка на нахождение книги в Избранном
+             * 
+             * @return boolean
+             */
             public function checkFav() {
-                if ($this->xml === FALSE) return;
+                if ($this->xml === FALSE) return FALSE;
                 foreach ($this->xml->book as $value) {
                     $this->id = $value->attributes();
                     if ($this->id['name'] == $_GET['id']) {
-                        $this->stat = TRUE;
-                        return;
+                        return TRUE;
                     }
                 }
             }
+            /**
+             * Вывод Избранных книг
+             * 
+             * @param MDB2 Объект с коннектом к БД
+             */
             public function showFav($mdb2) {
                 $this->print = '<h3 style="text-align: center">Увы, у Вас нет Избранных книг :-(</h3>';
                 if ($this->xml === FALSE) {
@@ -87,9 +118,21 @@ class FavBook {
                     }
                 }
             }
+            /**
+             * Возврат переменной stat
+             * 
+             * @return boolean
+             */
             public function getStat() {
                 return $this->stat;
             }
+            /**
+             * Проверка Избранной книги в БД
+             * 
+             * @param MDB2 Объект с коннектом к БД
+             * @param string Запрос
+             * @return int
+             */
             private function checkBookDB($mdb2, $query) {
                 $this->result = $mdb2->query($query);
                 return $this->result->numRows();
