d4419ac238590ad3bd23e603602a8cb425ce671e
Frolkin Nikita <frolkinnikita94@gmail.com>
2014-07-02 19:44:34 +0400

    Правки к документации

--- PageButtons.php
+++ PageButtons.php
@@ -16,62 +16,62 @@ class PageButtons {
     private $pageright = array();
     private $pervpage;
     private $nextpage;
-        /**
-         * Отправка запроса на выборку и высчитывание количества страниц
-         * 
-         * @param int Количество выводимых блоков
-         * @param string Запрос
-         * @param string Допольнительная часть GET-запроса
-         * @param MDB2 Объект коннекта к БД
-         */
-        public function __construct($num, $query, $inset , $mdb2) {
-            $this->page = $_GET['page'];
-            $this->result = $mdb2->query($query);
-            $this->posts = $this->result->numRows();
-            $this->inset = $inset;
-            $this->total = (($this->posts - 1) / $num) + 1;
-            $this->total = intval($this->total);
-            if (empty($this->page) || $this->page < 0) $this->page = 1;
-            if ($this->page > $this->total) $this->page = $this->total;
-        }
-        /**
-         * Вывод кнопок
-         */
-        public function getButtons() {
-            if ($this->page != 1) $this->pervpage = '<a href=?'.$this->inset.'&page=1>Первая</a> | <a href=?'.$this->inset.'&page='. ($this->page - 1) .'>Предыдущая</a> | ';
-            if ($this->page != $this->total) $this->nextpage = ' | <a href=?'.$this->inset.'&page='. ($this->page + 1) .'>Следующая</a> | <a href=?'.$this->inset.'&page=' .$this->total. '>Последняя</a>';
-            if ($this->page - 5 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 5) .'>'. ($this->page - 5) .'</a> | ';
-            if ($this->page - 4 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 4) .'>'. ($this->page - 4) .'</a> | ';
-            if ($this->page - 3 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 3) .'>'. ($this->page - 3) .'</a> | ';
-            if ($this->page - 2 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 2) .'>'. ($this->page - 2) .'</a> | ';
-            if ($this->page - 1 > 0) $this->pageleft[] = '<a href=?'.$this->inset.'&page='. ($this->page - 1) .'>'. ($this->page - 1) .'</a> | ';
-            if ($this->page + 1 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 1) .'>'. ($this->page + 1) .'</a>';
-            if ($this->page + 2 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 2) .'>'. ($this->page + 2) .'</a>';
-            if ($this->page + 3 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 3) .'>'. ($this->page + 3) .'</a>';
-            if ($this->page + 4 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 4) .'>'. ($this->page + 4) .'</a>';
-            if ($this->page + 5 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 5) .'>'. ($this->page + 5) .'</a>';
-            $this->setPageButtons();
-        }
-        /**
-         * Получение кнопок
-         */
-        private function setPageButtons() {
-            if ($this->total > 1)
-            {
-                $this->content = "<div class='pg_buttons'>";
-                $this->content .= $this->pervpage;
-                foreach ($this->pageleft as $value) {
-                    $this->content .= $value;
-                }
-                $this->content .= '<b>'.$this->page.'</b>';
-                foreach ($this->pageright as $value) {
-                    $this->content .= $value;
-                }
-                $this->content .= $this->nextpage;
-                $this->content .= "</div>";
-                echo $this->content;
-                $this->pageleft = array();
-                $this->pageright = array();
+    /**
+     * Отправка запроса на выборку и высчитывание количества страниц
+     * 
+     * @param int Количество выводимых блоков
+     * @param string Запрос
+     * @param string Допольнительная часть GET-запроса
+     * @param MDB2 Объект коннекта к БД
+     */
+    public function __construct($num, $query, $inset , $mdb2) {
+        $this->page = $_GET['page'];
+        $this->result = $mdb2->query($query);
+        $this->posts = $this->result->numRows();
+        $this->inset = $inset;
+        $this->total = (($this->posts - 1) / $num) + 1;
+        $this->total = intval($this->total);
+        if (empty($this->page) || $this->page < 0) $this->page = 1;
+        if ($this->page > $this->total) $this->page = $this->total;
+    }
+    /**
+     * Вывод кнопок
+     */
+    public function getButtons() {
+        if ($this->page != 1) $this->pervpage = '<a href=?'.$this->inset.'&page=1>Первая</a> | <a href=?'.$this->inset.'&page='. ($this->page - 1) .'>Предыдущая</a> | ';
+        if ($this->page != $this->total) $this->nextpage = ' | <a href=?'.$this->inset.'&page='. ($this->page + 1) .'>Следующая</a> | <a href=?'.$this->inset.'&page=' .$this->total. '>Последняя</a>';
+        if ($this->page - 5 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 5) .'>'. ($this->page - 5) .'</a> | ';
+        if ($this->page - 4 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 4) .'>'. ($this->page - 4) .'</a> | ';
+        if ($this->page - 3 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 3) .'>'. ($this->page - 3) .'</a> | ';
+        if ($this->page - 2 > 0) $this->pageleft[] = ' <a href=?'.$this->inset.'&page='. ($this->page - 2) .'>'. ($this->page - 2) .'</a> | ';
+        if ($this->page - 1 > 0) $this->pageleft[] = '<a href=?'.$this->inset.'&page='. ($this->page - 1) .'>'. ($this->page - 1) .'</a> | ';
+        if ($this->page + 1 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 1) .'>'. ($this->page + 1) .'</a>';
+        if ($this->page + 2 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 2) .'>'. ($this->page + 2) .'</a>';
+        if ($this->page + 3 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 3) .'>'. ($this->page + 3) .'</a>';
+        if ($this->page + 4 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 4) .'>'. ($this->page + 4) .'</a>';
+        if ($this->page + 5 <= $this->total) $this->pageright[] = ' | <a href=?'.$this->inset.'&page='. ($this->page + 5) .'>'. ($this->page + 5) .'</a>';
+        $this->setPageButtons();
+    }
+    /**
+     * Получение кнопок
+     */
+    private function setPageButtons() {
+        if ($this->total > 1)
+        {
+            $this->content = "<div class='pg_buttons'>";
+            $this->content .= $this->pervpage;
+            foreach ($this->pageleft as $value) {
+                $this->content .= $value;
             }
+            $this->content .= '<b>'.$this->page.'</b>';
+            foreach ($this->pageright as $value) {
+                $this->content .= $value;
+            }
+            $this->content .= $this->nextpage;
+            $this->content .= "</div>";
+            echo $this->content;
+            $this->pageleft = array();
+            $this->pageright = array();
         }
+    }
 }
--- ShowNews.php
+++ ShowNews.php
@@ -8,50 +8,50 @@
 class ShowNews {
     private $result;
     private $query;
-        /**
-         * Отправка запроса на выборку
-         * 
-         * @param string Запрос
-         * @param MDB2 Объект коннекта к БД
-         * @param int С какой позиции делать выборку
-         * @param int Сколько записей включать в запрос
-         */
-        public function __construct($query, $mdb2, $min_limit=0, $max_limit=3) {
-            $this->query = $query . " LIMIT $min_limit, $max_limit";
-            $this->result = $mdb2->query($this->query);
-        }
-        /**
-         * Вывод новостей сайта
-         */
-        public function ShowNews() {
-            if ($this->result->numRows() > 0)
+    /**
+     * Отправка запроса на выборку
+     * 
+     * @param string Запрос
+     * @param MDB2 Объект коннекта к БД
+     * @param int С какой позиции делать выборку
+     * @param int Сколько записей включать в запрос
+     */
+    public function __construct($query, $mdb2, $min_limit=0, $max_limit=3) {
+        $this->query = $query . " LIMIT $min_limit, $max_limit";
+        $this->result = $mdb2->query($this->query);
+    }
+    /**
+     * Вывод новостей сайта
+     */
+    public function ShowNews() {
+        if ($this->result->numRows() > 0)
+        {
+            while ($this->row = $this->result->fetchRow())
             {
-                while ($this->row = $this->result->fetchRow())
-                {
-                    print "<div class='news'>
-                                    <h1>".  $this->row['header']."</h1>
-                                    <p>".  $this->row['desc']."</p>
-                               </div>";
-                }
-            } else print "<h3>Пока новостей нет</h3>";
-        }
-        /**
-         * Вывод новостей книг
-         */
-        public function ShowBooks() {
-            if ($this->result->numRows() > 0)
+                print "<div class='news'>
+                                <h1>".  $this->row['header']."</h1>
+                                <p>".  $this->row['desc']."</p>
+                           </div>";
+            }
+        } else print "<h3>Пока новостей нет</h3>";
+    }
+    /**
+     * Вывод новостей книг
+     */
+    public function ShowBooks() {
+        if ($this->result->numRows() > 0)
+        {
+            while ($this->row = $this->result->fetchRow())
             {
-                while ($this->row = $this->result->fetchRow())
-                {
-                    print "<div class='newble'>
-                                <a href='page.php?id=".  $this->row['id']."'><img src='uploads/".  $this->row['img'].'.jpeg'."' alt='картинка'>
-                                <div class='description'>
-                                    <h1>".  $this->row['book_name']."</h1>
-                                    <h3>".  $this->row['author']."</h3>
-                                </div>
-                                <div class='clearfix'></div></a>
-                            </div>";
-                }
-            } else print "<h3>Новых книг пока нет</h3>";
-        }
+                print "<div class='newble'>
+                            <a href='page.php?id=".  $this->row['id']."'><img src='uploads/".  $this->row['img'].'.jpeg'."' alt='картинка'>
+                            <div class='description'>
+                                <h1>".  $this->row['book_name']."</h1>
+                                <h3>".  $this->row['author']."</h3>
+                            </div>
+                            <div class='clearfix'></div></a>
+                        </div>";
+            }
+        } else print "<h3>Новых книг пока нет</h3>";
+    }
 }
