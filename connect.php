329cf05d41f5b1190fd689fa28186f9f8e7303ca
Frolkin Nikita <frolkinnikita94@gmail.com>
2014-07-03 13:13:36 +0400

    Ничего не произошло, этого коммита нет

--- connect.php
+++ connect.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'MDB2.php';
+//ini_set('display_errors', 'Off'); //выключение отображение ошибок
 $dsn = 'mysql://user:d8YAEwatyCB7JNB2@library.ru/library';
 $mdb2 = MDB2::connect($dsn);
 if (PEAR::isError($mdb2))
 {
     die($mdb2->getMessage());
+} else {
+    $mdb2->setFetchMode(MDB2_FETCHMODE_ASSOC);
 }
-$mdb2->setFetchMode(MDB2_FETCHMODE_ASSOC);
 ?>
